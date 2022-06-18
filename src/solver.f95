ed7aae3db4b6ee6f4c2f29d48fc0aef7f3c0736f
William Dawn <wcdawn@ncsu.edu>
2022-06-17 20:38:13 -0600

    outline for linear system solver

--- src/solver.f95
+++ src/solver.f95
@@ -1,9 +1,25 @@
-! TODO sor solver
+! TODO finish implementation
+subroutine sor(aa, b, x, rank, omega, tol_inner_x, tol_inner_maxit)
+  use precisions, only : ki4, kr8
+  IMPLICIT NONE
+  ! designed for a 5-stripe matrix with the diagonal in the fifth position
+  real(kr8), intent(in) :: aa(:,:) ! (5, rank)
+  real(kr8), intent(in) :: b(:)    ! (rank)
+  real(kr8), intent(inout) :: x(:)    ! (rank)
+  integer(ki4), intent(in) :: rank
+  real(kr8), intent(in) :: omega
+  real(kr8), intent(in) :: tol_inner_x
+  integer(ki4), intent(in) :: tol_inner_maxit
 
-! TODO proper outputs... (log & stdout)
+  integer(ki4) :: i, iter
+
+  do iter = 1,tol_inner_maxit
+  enddo
+
+endsubroutine sor
 
 subroutine solver()
-  use globals, only : stdout_unit, &
+  use globals, only : stdout_unit, & ! TODO output to log as well
     core_x_size, core_y_size, &
     xkeff, xflux, tol_max_iter, tol_xkeff, tol_xflux
   use precisions, only : ki4, kr8
@@ -21,14 +37,14 @@ subroutine solver()
   ! TODO write a routine to build bvec
   allocate(bvec(core_x_size*core_y_size*2)) ! 2 energy groups
 
-  write(stdout_unit, '(a)') 'Iter Keff Conv_Keff Conv_Flux'
+  write(stdout_unit, '(a)') 'Iter | Keff     | Conv_Keff | Conv_Flux'
 
   conv_xflux = 1d2*tol_xflux + 1d0
   conv_xkeff = 1d2*tol_xkeff + 1d0
 
   do iter = 1,tol_max_iter
 
-    write(stdout_unit, '(i3, x, f8.6, x, es8.2, x, es8.2)') &
+    write(stdout_unit, '(i4, 3x, f8.6, 3x, es8.2, 4x, es8.2)') &
       iter, xkeff, conv_xkeff, conv_xflux
 
     if ((conv_xflux < tol_xflux) .and. (conv_xkeff < tol_xkeff)) exit
