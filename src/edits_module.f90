e83607aff69d23061d478847305a34e8a1add293
William Dawn <wcdawn@ncsu.edu>
2022-08-09 18:46:26 -0600

    fixing capitalization

--- src/edits_module.f90
+++ src/edits_module.f90
@@ -2,36 +2,36 @@
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 !> @brief Module for performing edits
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
-module edits_module
+MODULE edits_module
 IMPLICIT NONE
 
-contains
+CONTAINS
 
 !---------------------------------------------------------------------------------------------------
 !> @brief This subroutine edits the cross sections, calculating the kinf for each assembly
 !>
-  subroutine edit_xs()
+  SUBROUTINE edit_xs()
     USE string_module, ONLY : str
-    use xs_types, only : macro_assm_xs_type
-    use precisions, only : ki4, kr8
-    use globals, only : stdout_unit, num_assm_reg, assm_xs, num_eg, print_log
-    use errors_module, only : fatal_error
+    USE xs_types, ONLY : macro_assm_xs_type
+    USE precisions, ONLY : ki4, kr8
+    USE globals, ONLY : stdout_unit, num_assm_reg, assm_xs, num_eg, print_log
+    USE errors_module, ONLY : fatal_error
     IMPLICIT NONE
 
-    integer(ki4) :: i
-    real(kr8) :: flux_ratio, kinf
+    INTEGER(ki4) :: i
+    REAL(kr8) :: flux_ratio, kinf
 
-    if (num_eg /= 2) call fatal_error('only supporting 2 energy groups')
+    IF (num_eg /= 2) CALL fatal_error('ONLY supporting 2 energy groups')
 
-    do i = 1,num_assm_reg
-      if (assm_xs(i)%fissile) then
+    DO i = 1,num_assm_reg
+      IF (assm_xs(i)%fissile) THEN
         flux_ratio = assm_xs(i)%sigma_scat(2,1)/assm_xs(i)%sigma_a(2)
         kinf = (assm_xs(i)%nusigma_f(1) + assm_xs(i)%nusigma_f(2)*flux_ratio) / &
           (assm_xs(i)%sigma_a(1) + assm_xs(i)%sigma_scat(2,1))
-        CALL print_log('kinf '//trim(adjustl(assm_xs(i)%mat_id))//' '//str(kinf,6,'F'))
-      endif
-    enddo
+        CALL print_log('kinf '//TRIM(ADJUSTL(assm_xs(i)%mat_id))//' '//str(kinf,6,'F'))
+      ENDIF
+    ENDDO
 
-  endsubroutine edit_xs
+  ENDSUBROUTINE edit_xs
 
-endmodule edits_module
+ENDMODULE edits_module
